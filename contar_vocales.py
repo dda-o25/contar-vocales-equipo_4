ea69b937e7ad89cb8d5eac48264f2aed16830818
chrisfb1901 <christian.flores@iteso.mx>
2025-10-06 20:31:08 -0600

    contar_vocales

--- contar_vocales.py
+++ contar_vocales.py
@@ -1,15 +1,30 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+
+ANGELICA ELIZABETH IBARRA DIAZ
+ DIAZARTURO ARJONA HERMOSILLOA
+ CHRISTIAN ALONSO FLORES BURGOS
+ MARCO FABRIZIO AZPEITIA CASTELLANOS
+
+ 10 de octubre 2025
+
+ Contar la cantidad de vocales en una frase
 """
 
 # Declaraciones
-CONSTANTE = valor
+vocal="aáeéiíoóuúü"
 
 # Entradas
-entrada = input()
+frase = input("Introduce una frase")
+contador = 0
+x = 0
 
 # Proceso
+frase = frase.lower()
 
+while x < len(frase):
+    if frase[x] in vocal:
+        contador += 1
+    x += 1
 
 # Salidas
-print(salida)
+print(f"La frase tiene {contador} vocales")
